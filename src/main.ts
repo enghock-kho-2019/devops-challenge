01534a462a37a84107dd50fb0f2ee788c3fb0051
Kho Eng Hock <>
2022-03-08 12:12:09 +0800

    Changed platform to Docker in AWS beanstalk

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Uses an environment variable for port number or defaults to 3000
-  
   const port = process.env.PORT || 3000;
   await app.listen(port);
 }
